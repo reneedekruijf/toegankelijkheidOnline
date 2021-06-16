978caddd628fbddb8d56c66a07f201e782a70269
Renee de Kruijf <renee@webdesignpuntnl.com>
2021-06-16 10:35:43 +0200

    added google analytics

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', 'UA-63079670-2', { 'anonymize_ip': true });
+gtag('config', 'UA-63079670-6', { 'anonymize_ip': true });
